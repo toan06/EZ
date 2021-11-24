fb6c6ba15411791c68ffc143a171f1755348845c
toan06 <94643704+toan06@users.noreply.github.com>
2021-11-25 04:45:18 +0700

    Update code_omegle_v1.js

--- Omegle/code_omegle_v1.js
+++ Omegle/code_omegle_v1.js
@@ -1,5 +1,5 @@
-const popup_get_text = prompt("   text ","any text");
-const popup_get_time = prompt("   time 1 = 1s, 0.5 = 0,5s... ","0.5");
+var popup_get_text = prompt("   text ","any text");
+var popup_get_time = prompt("   time 1 = 1s, 0.5 = 0,5s... ","0.5");
 function spam_Omegle(){
 	const $ = document.querySelector.bind(document);
 	const newBtn = $(".disconnectbtn");
@@ -9,4 +9,4 @@ function spam_Omegle(){
 	chatBox.value = popup_get_text;
 	sendBtn.click();
 };
-setInterval(spam_Omegle,popup_get_time*1000)
+setInterval(spam_Omegle,popup_get_time*1000); 
